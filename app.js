4f731a9ba9cf78c4ee6cb7b7c1f97c398b9c3867
inga0101 <ingaa134@gmail.com>
2021-03-23 11:42:27 +0100

    fetch api

--- app.js
+++ app.js
@@ -1,40 +1,13 @@
-    
-// const activity = document.querySelector('#result');
 
-// const fetchData = async (id) => {
-//     const res = await fetch('https://www.boredapi.com/api/activity?participants=:participants');
-//     console.log(fetch);
-
-//     const result = await res.json();
-//     renderData(result);
-
-// };
-
-// fetchData();
-const activity = document.querySelector('#result')
-
-
-async function fetchData () {
-    const res = await fetch(`http://www.boredapi.com/api/`);
+async function fetchActivity () {
+    const res = await fetch
+     ('http://www.boredapi.com/api/activity?participants');
  
      const data = await res.json ();
-     renderData(result);
  
-     console.log(id);
+     console.log(data);
  
  }
  
- fetchData();
+ fetchActivity();
 
-//  function getActivity() {
-
-//  }
-
- const renderData = data => {
-     data.map(value =>{
-         const li = document.createElement('li');
-         li.innerHTML = `activity:${value.activity} participants:${value.participants}`;
-         activity.appendChild(li);
-         console.log(li);
-     });
- };
